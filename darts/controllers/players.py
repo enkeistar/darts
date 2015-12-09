d40d43deaae66e89c501e3dafde24a28d13aa1b0
brettalanmeyer <brettalanmeyer@gmail.com>
2015-12-09 15:49:17 -0600

    Added player details/stats.

--- darts/controllers/players.py
+++ darts/controllers/players.py
@@ -1,13 +1,13 @@
 from darts import app
 from flask import Blueprint, request, render_template, redirect
-from darts.entities import player
+from darts.entities import player as playerModel, game as gameModel, team_player as teamPlayerModel, score as scoreModel
 from darts import model
 
 mod = Blueprint("players", __name__, url_prefix = "/players")
 
 @mod.route("/", methods = ["GET"])
 def players_index():
-	players = model.Model().select(player.Player)
+	players = model.Model().select(playerModel.Player)
 	return render_template("players/index.html", players = players)
 
 @mod.route("/new/", methods = ["GET"])
@@ -16,25 +16,28 @@ def players_new():
 
 @mod.route("/", methods = ["POST"])
 def players_create():
-	newPlayer = player.Player(request.form["name"])
+	newPlayer = playerModel.Player(request.form["name"])
 	model.Model().create(newPlayer)
 	return redirect("/players/")
 
 @mod.route("/<int:id>/", methods = ["GET"])
 def players_details(id):
-	return "details"
+	player = model.Model().selectById(playerModel.Player, id)
+	teams = model.Model().select(teamPlayerModel.TeamPlayer).filter_by(playerId = id)
+	scores = model.Model().select(scoreModel.Score).filter_by(playerId = id)
+	return render_template("players/details.html", player = player, teams = teams, scores = scores)
 
 @mod.route("/<int:id>/edit/", methods = ["GET"])
 def players_edit(id):
-	players = model.Model().selectById(player.Player, id)
+	players = model.Model().selectById(playerModel.Player, id)
 	return render_template("players/edit.html", player = players)
 
 @mod.route("/<int:id>/", methods = ["POST"])
 def players_update(id):
-	model.Model().update(player.Player, id, request.form)
+	model.Model().update(playerModel.Player, id, request.form)
 	return redirect("/players/")
 
 @mod.route("/<int:id>/delete/", methods = ["POST"])
 def players_delete(id):
-	model.Model().delete(player.Player, id)
+	model.Model().delete(playerModel.Player, id)
 	return redirect("/players/")
