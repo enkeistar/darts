9bdef123dd08cc47b65fcc1b8f137cbc4383bd26
brettalanmeyer <brettalanmeyer@gmail.com>
2015-12-11 11:55:50 -0600

    Create script.

---
+++ darts/db/create.sql
@@ -0,0 +1,56 @@
+
+CREATE TABLE IF NOT EXISTS `games` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `players` int(11) DEFAULT NULL,
+  `round` int(11) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `players` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `teams` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `gameId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_teams_games` (`gameId`),
+  CONSTRAINT `fk_teams_games` FOREIGN KEY (`gameId`) REFERENCES `games` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `scores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `gameId` int(11) DEFAULT NULL,
+  `teamId` int(11) DEFAULT NULL,
+  `playerId` int(11) DEFAULT NULL,
+  `round` int(11) DEFAULT NULL,
+  `twenty` tinyint(4) DEFAULT NULL,
+  `nineteen` tinyint(4) DEFAULT NULL,
+  `eighteen` tinyint(4) DEFAULT NULL,
+  `seventeen` tinyint(4) DEFAULT NULL,
+  `sixteen` tinyint(4) DEFAULT NULL,
+  `fifteen` tinyint(4) DEFAULT NULL,
+  `bullseye` tinyint(4) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_scores_games` (`gameId`),
+  KEY `fk_scores_teams` (`teamId`),
+  KEY `fk_scores_players` (`playerId`),
+  CONSTRAINT `fk_scores_games` FOREIGN KEY (`gameId`) REFERENCES `games` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_scores_players` FOREIGN KEY (`playerId`) REFERENCES `players` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_scores_teams` FOREIGN KEY (`teamId`) REFERENCES `teams` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `teams_players` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `teamId` int(11) DEFAULT NULL,
+  `playerId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_teams_players_players` (`teamId`),
+  KEY `fk_teams_players_teams` (`playerId`),
+  CONSTRAINT `fk_teams_players_players` FOREIGN KEY (`playerId`) REFERENCES `players` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_teams_players_teams` FOREIGN KEY (`teamId`) REFERENCES `teams` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
