28b05a339662b5fd146ae0838cfbe68780bd2729
brettalanmeyer <brettalanmeyer@gmail.com>
2016-01-04 10:29:47 -0600

    Moved scoring to results table.

--- darts/controllers/games.py
+++ darts/controllers/games.py
@@ -42,24 +42,6 @@ def games_index():
 				user = model.Model().selectById(playerModel.Player, player.playerId)
 				teamData["players"].append(user.name)
 
-			marks = model.Model().select(markModel.Mark).filter_by(gameId = game.id, teamId = team.id)
-
-			for mark in marks:
-				if mark.twenty:
-					teamData["mark"] += 20
-				elif mark.nineteen:
-					teamData["mark"] += 19
-				elif mark.eighteen:
-					teamData["mark"] += 18
-				elif mark.seventeen:
-					teamData["mark"] += 17
-				elif mark.sixteen:
-					teamData["mark"] += 16
-				elif mark.fifteen:
-					teamData["mark"] += 15
-				elif mark.bullseye:
-					teamData["mark"] += 25
-
 			gameData["teams"].append(teamData)
 
 		data.append(gameData)
@@ -72,7 +54,7 @@ def games_index():
 def games_board(id):
 	game = model.Model().selectById(gameModel.Game, id)
 	teams = model.Model().select(teamModel.Team).filter_by(gameId = game.id)
-	results = model.Model().select(resultModel.Result).filter_by(gameId = game.id)
+
 
 	data = {
 		"id": int(game.id),
@@ -80,8 +62,7 @@ def games_board(id):
 		"round": game.round,
 		"players": game.players,
 		"turn": game.turn,
-		"teams": [],
-		"results": results
+		"teams": []
 	}
 
 	for team in teams:
@@ -89,7 +70,7 @@ def games_board(id):
 		teamData = {
 			"id": team.id,
 			"players": [],
-			"marks": [{
+			"marks": {
 				20: 0,
 				19: 0,
 				18: 0,
@@ -98,26 +79,25 @@ def games_board(id):
 				15: 0,
 				25: 0,
 				"points": 0
-			}, {
-				20: 0,
-				19: 0,
-				18: 0,
-				17: 0,
-				16: 0,
-				15: 0,
-				25: 0,
-				"points": 0
-			}, {
-				20: 0,
-				19: 0,
-				18: 0,
-				17: 0,
-				16: 0,
-				15: 0,
-				25: 0,
-				"points": 0
-			}]
+			},
+			"results": []
 		}
+
+		for i in range(1, 4):
+			results = model.Model().select(resultModel.Result).filter_by(gameId = game.id, teamId = team.id, game = i)
+			resultSet = {
+				"score": 0,
+				"win": 0,
+				"loss": 0
+			}
+
+			if results.count() > 0:
+				result = results.first()
+				resultSet["score"] = result.score
+				resultSet["win"] = result.win
+				resultSet["loss"] = result.loss
+
+			teamData["results"].append(resultSet)
 
 		players = model.Model().select(teamPlayerModel.TeamPlayer).filter_by(teamId = team.id)
 
@@ -130,59 +110,57 @@ def games_board(id):
 				"name": user.name
 			})
 
-		for i in range(0,3):
+		scored = {
+			"twenty": 0,
+			"nineteen": 0,
+			"eighteen": 0,
+			"seventeen": 0,
+			"sixteen": 0,
+			"fifteen": 0,
+			"bullseye": 0
+		}
+		pointsScored = 0
 
-			scored = {
-				"twenty": 0,
-				"nineteen": 0,
-				"eighteen": 0,
-				"seventeen": 0,
-				"sixteen": 0,
-				"fifteen": 0,
-				"bullseye": 0
-			}
-			pointsScored = 0
+		marks = model.Model().select(markModel.Mark).filter_by(gameId = game.id, teamId = team.id, game = game.game)
 
-			marks = model.Model().select(markModel.Mark).filter_by(gameId = game.id, teamId = team.id, game = i + 1)
+		for mark in marks:
+			if mark.twenty:
+				teamData["marks"][20] += 1
+				scored["twenty"] += 1
+				if scored["twenty"] > 3:
+					pointsScored += 20
+			elif mark.nineteen:
+				teamData["marks"][19] += 1
+				scored["nineteen"] += 1
+				if scored["nineteen"] > 3:
+					pointsScored += 19
+			elif mark.eighteen:
+				teamData["marks"][18] += 1
+				scored["eighteen"] += 1
+				if scored["eighteen"] > 3:
+					pointsScored += 18
+			elif mark.seventeen:
+				teamData["marks"][17] += 1
+				scored["seventeen"] += 1
+				if scored["seventeen"] > 3:
+					pointsScored += 17
+			elif mark.sixteen:
+				teamData["marks"][16] += 1
+				scored["sixteen"] += 1
+				if scored["sixteen"] > 3:
+					pointsScored += 16
+			elif mark.fifteen:
+				teamData["marks"][15] += 1
+				scored["fifteen"] += 1
+				if scored["fifteen"] > 3:
+					pointsScored += 15
+			elif mark.bullseye:
+				teamData["marks"][25] += 1
+				scored["bullseye"] += 1
+				if scored["bullseye"] > 3:
+					pointsScored += 25
 
-			for mark in marks:
-				if mark.twenty:
-					teamData["marks"][i][20] += 1
-					scored["twenty"] += 1
-					if scored["twenty"] > 3:
-						pointsScored += 20
-				elif mark.nineteen:
-					teamData["marks"][i][19] += 1
-					scored["nineteen"] += 1
-					if scored["nineteen"] > 3:
-						pointsScored += 19
-				elif mark.eighteen:
-					teamData["marks"][i][18] += 1
-					scored["eighteen"] += 1
-					if scored["eighteen"] > 3:
-						pointsScored += 18
-				elif mark.seventeen:
-					teamData["marks"][i][17] += 1
-					scored["seventeen"] += 1
-					if scored["seventeen"] > 3:
-						pointsScored += 17
-				elif mark.sixteen:
-					teamData["marks"][i][16] += 1
-					scored["sixteen"] += 1
-					if scored["sixteen"] > 3:
-						pointsScored += 16
-				elif mark.fifteen:
-					teamData["marks"][i][15] += 1
-					scored["fifteen"] += 1
-					if scored["fifteen"] > 3:
-						pointsScored += 15
-				elif mark.bullseye:
-					teamData["marks"][i][25] += 1
-					scored["bullseye"] += 1
-					if scored["bullseye"] > 3:
-						pointsScored += 25
-
-			teamData["marks"][i]["points"] = pointsScored
+		teamData["marks"]["points"] = pointsScored
 
 		data["teams"].append(teamData)
 
--- darts/db/create.sql
+++ darts/db/create.sql
@@ -1,25 +1,12 @@
-
 CREATE TABLE IF NOT EXISTS `games` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `players` int(11) DEFAULT NULL,
   `game` int(11) DEFAULT NULL,
+  `round` int(11) DEFAULT NULL,
   `ready` tinyint(4) DEFAULT NULL,
+  `turn` int(11) DEFAULT NULL,
   `createdAt` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE IF NOT EXISTS `players` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE IF NOT EXISTS `teams` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `gameId` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_teams_games` (`gameId`),
-  CONSTRAINT `fk_teams_games` FOREIGN KEY (`gameId`) REFERENCES `games` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS `marks` (
@@ -38,18 +25,50 @@ CREATE TABLE IF NOT EXISTS `marks` (
   `bullseye` tinyint(4) DEFAULT NULL,
   `createdAt` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_marks_games` (`gameId`),
-  KEY `fk_marks_teams` (`teamId`),
-  KEY `fk_marks_players` (`playerId`),
-  CONSTRAINT `fk_marks_games` FOREIGN KEY (`gameId`) REFERENCES `games` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_marks_players` FOREIGN KEY (`playerId`) REFERENCES `players` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_marks_teams` FOREIGN KEY (`teamId`) REFERENCES `teams` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `fk_scores_games` (`gameId`),
+  KEY `fk_scores_teams` (`teamId`),
+  KEY `fk_scores_players` (`playerId`),
+  CONSTRAINT `fk_scores_games` FOREIGN KEY (`gameId`) REFERENCES `games` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_scores_players` FOREIGN KEY (`playerId`) REFERENCES `players` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_scores_teams` FOREIGN KEY (`teamId`) REFERENCES `teams` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `players` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `results` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `gameId` int(11) DEFAULT NULL,
+  `teamId` int(11) DEFAULT NULL,
+  `game` int(11) DEFAULT NULL,
+  `score` int(11) DEFAULT NULL,
+  `win` tinyint(1) DEFAULT NULL,
+  `loss` tinyint(1) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_results_games` (`gameId`),
+  KEY `fk_results_teams` (`teamId`),
+  CONSTRAINT `fk_results_games` FOREIGN KEY (`gameId`) REFERENCES `games` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_results_teams` FOREIGN KEY (`teamId`) REFERENCES `teams` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `teams` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `gameId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_teams_games` (`gameId`),
+  CONSTRAINT `fk_teams_games` FOREIGN KEY (`gameId`) REFERENCES `games` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS `teams_players` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `teamId` int(11) DEFAULT NULL,
   `playerId` int(11) DEFAULT NULL,
+  `win` tinyint(1) DEFAULT NULL,
+  `loss` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_teams_players_players` (`teamId`),
   KEY `fk_teams_players_teams` (`playerId`),
