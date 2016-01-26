7deb77b4aa71e485c9609f82f51c87b83458bc95
brettalanmeyer <brettalanmeyer@gmail.com>
2016-01-26 13:32:00 -0600

    Added change script.

---
+++ darts/db/update-20160126132700.sql
@@ -0,0 +1,21 @@
+
+ALTER TABLE `marks` ADD COLUMN `value` INT(11) NULL DEFAULT NULL AFTER `bullseye`;
+
+UPDATE `marks` SET `value` = 25 WHERE `bullseye` = 1;
+UPDATE `marks` SET `value` = 20 WHERE `twenty` = 1;
+UPDATE `marks` SET `value` = 19 WHERE `nineteen` = 1;
+UPDATE `marks` SET `value` = 18 WHERE `eighteen` = 1;
+UPDATE `marks` SET `value` = 17 WHERE `seventeen` = 1;
+UPDATE `marks` SET `value` = 16 WHERE `sixteen` = 1;
+UPDATE `marks` SET `value` = 15 WHERE `fifteen` = 1;
+UPDATE `marks` SET `value` = 0 WHERE `value` IS NULL;
+
+
+ALTER TABLE `marks`
+	DROP COLUMN `twenty`,
+	DROP COLUMN `nineteen`,
+	DROP COLUMN `eighteen`,
+	DROP COLUMN `seventeen`,
+	DROP COLUMN `sixteen`,
+	DROP COLUMN `fifteen`,
+	DROP COLUMN `bullseye`;
