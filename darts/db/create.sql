23628886c21cc75339af7570c993e268e322e760
brettalanmeyer <brettalanmeyer@gmail.com>
2016-01-26 10:31:11 -0600

    Updated with comments and no-fk attributes.

--- darts/db/create.sql
+++ darts/db/create.sql
@@ -1,3 +1,16 @@
+-- --------------------------------------------------------
+-- Host:                         10.9.0.160
+-- Server version:               5.5.46-0ubuntu0.14.04.2 - (Ubuntu)
+-- Server OS:                    debian-linux-gnu
+-- HeidiSQL Version:             9.3.0.4984
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping structure for table darts-production.games
 CREATE TABLE IF NOT EXISTS `games` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `modeId` int(11) DEFAULT NULL,
@@ -13,6 +26,10 @@ CREATE TABLE IF NOT EXISTS `games` (
   CONSTRAINT `fk_games_modes` FOREIGN KEY (`modeId`) REFERENCES `modes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table darts-production.marks
 CREATE TABLE IF NOT EXISTS `marks` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `gameId` int(11) DEFAULT NULL,
@@ -37,6 +54,10 @@ CREATE TABLE IF NOT EXISTS `marks` (
   CONSTRAINT `fk_scores_teams` FOREIGN KEY (`teamId`) REFERENCES `teams` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table darts-production.modes
 CREATE TABLE IF NOT EXISTS `modes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
@@ -45,6 +66,10 @@ CREATE TABLE IF NOT EXISTS `modes` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table darts-production.players
 CREATE TABLE IF NOT EXISTS `players` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
@@ -52,6 +77,10 @@ CREATE TABLE IF NOT EXISTS `players` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table darts-production.results
 CREATE TABLE IF NOT EXISTS `results` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `gameId` int(11) DEFAULT NULL,
@@ -68,6 +97,10 @@ CREATE TABLE IF NOT EXISTS `results` (
   CONSTRAINT `fk_results_teams` FOREIGN KEY (`teamId`) REFERENCES `teams` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table darts-production.teams
 CREATE TABLE IF NOT EXISTS `teams` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `gameId` int(11) DEFAULT NULL,
@@ -78,6 +111,10 @@ CREATE TABLE IF NOT EXISTS `teams` (
   CONSTRAINT `fk_teams_games` FOREIGN KEY (`gameId`) REFERENCES `games` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table darts-production.teams_players
 CREATE TABLE IF NOT EXISTS `teams_players` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `teamId` int(11) DEFAULT NULL,
@@ -88,3 +125,8 @@ CREATE TABLE IF NOT EXISTS `teams_players` (
   CONSTRAINT `fk_teams_players_players` FOREIGN KEY (`playerId`) REFERENCES `players` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_teams_players_teams` FOREIGN KEY (`teamId`) REFERENCES `teams` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
