5aca7941633720913f726c876091674ba015dc51
brettalanmeyer <brettalanmeyer@gmail.com>
2016-01-29 09:45:39 -0600

    Refactored query session lifecycle.

--- darts/model.py
+++ darts/model.py
@@ -14,29 +14,40 @@ app.config.from_pyfile("config_file.cfg")
 engine = create_engine("mysql+mysqldb://" + app.config["MYSQL_USERNAME"] + ":" + app.config["MYSQL_PASSWORD"] + "@" + app.config["MYSQL_HOST"] + "/" + app.config["MYSQL_DATABASE"])
 db_session = scoped_session(sessionmaker(autocommit = False, autoflush = False, bind = engine))
 Session = sessionmaker(bind = engine)
-session = Session()
 
 class Model():
 
 	def select(self, model):
-		return session.query(model)
+		session = Session()
+		data = session.query(model)
+		session.close()
+		return data
 
 	def selectById(self, model, id):
-		return session.query(model).filter(model.id == id).one()
+		session = Session()
+		data = session.query(model).filter(model.id == id).one()
+		session.close()
+		return data
 
 	def create(self, data):
+		session = Session()
 		session.add(data)
 		session.commit()
+		session.close()
 		return data
 
 	def update(self, model, id, data):
+		session = Session()
 		session.query(model).filter(model.id == id).update(data)
 		session.commit()
+		session.close()
 		return data
 
 	def delete(self, model, id):
+		session = Session()
 		session.query(model).filter(model.id == id).delete()
 		session.commit()
+		session.close()
 
 	def getSession(self):
-		return session
+		return Session()
