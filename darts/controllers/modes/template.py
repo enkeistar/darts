8175756ec0a8bb038e12852d48d1aaab4c47d02e
brettalanmeyer <brettalanmeyer@gmail.com>
2016-03-31 14:03:27 -0500

    Updated text to matches.

--- darts/controllers/modes/template.py
+++ darts/controllers/modes/template.py
@@ -1,7 +1,7 @@
 from darts import app
 from flask import render_template
 
-@app.route("/games/<int:id>/modes/template/")
+@app.route("/matches/<int:id>/modes/template/")
 def template_index(id):
 	return "HELLO"
 
