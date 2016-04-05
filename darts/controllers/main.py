e7ae3f7d0d6867de421195e227f0440daf61016f
brettalanmeyer <brettalanmeyer@gmail.com>
2016-04-05 09:32:24 -0500

    Display ip address.

--- darts/controllers/main.py
+++ darts/controllers/main.py
@@ -1,7 +1,11 @@
 from darts import app
-from flask import Blueprint, render_template
+from flask import render_template, request
 
 @app.route("/")
 def main_index():
 	return render_template("main/index.html")
+
+@app.route("/test/")
+def main_test():
+	return request.remote_addr
 
